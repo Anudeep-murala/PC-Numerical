689fb64fa0ff5e576024bc4cbb3ffe3853a911ac
Anudeep-murala <122833633+Anudeep-murala@users.noreply.github.com>
2023-01-23 19:20:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,12 +25,12 @@ int main(int argc, char *argv[])
     std::cerr << "usage: " << argv[0] << " <functionid> <a> <b> <n> <intensity>" << std::endl;
     return -1;
   }
-  char* character;
-  float fId = strtof(argv[1],&character);
-  float a = strtof(argv[2],&character);
-  float b = strtof(argv[3],&character);
-  int n = strtof(argv[4],&character);
-  int intensity = strtof(argv[5],&character);
+  char *character;
+  float fId = strtof(argv[1], &character);
+  float a = strtof(argv[2], &character);
+  float b = strtof(argv[3], &character);
+  int n = strtof(argv[4], &character);
+  int intensity = strtof(argv[5], &character);
   std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
   float partOne = (b - a) / n;
   float finalOutput = 0;
@@ -38,29 +38,29 @@ int main(int argc, char *argv[])
   for (int i = 0; i < n; i++)
   {
     partTwo = a + ((i + 0.5) * partOne);
-    switch ((int) fId)
+    switch ((int)fId)
     {
     case 1:
-      finalOutput = finalOutput + f1(partTwo,intensity);
-          break;
+      finalOutput = finalOutput + f1(partTwo, intensity);
+      break;
     case 2:
-      finalOutput = finalOutput + f2(partTwo,intensity);
+      finalOutput = finalOutput + f2(partTwo, intensity);
       break;
     case 3:
-      finalOutput = finalOutput + f3(partTwo,intensity);
+      finalOutput = finalOutput + f3(partTwo, intensity);
       break;
     case 4:
-      finalOutput = finalOutput + f4(partTwo,intensity);
+      finalOutput = finalOutput + f4(partTwo, intensity);
       break;
     default:
-          std::cerr<<"Wrong Function ID."<<std::endl;
+      std::cerr << "Wrong Function ID." << std::endl;
       break;
-    }  
+    }
   }
-finalOutput*= partOne;
-std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
-std::chrono::duration<double, std::milli> difference = end - start;
-  std::cout<< finalOutput<<std::endl;
+  finalOutput *= partOne;
+  std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+  std::chrono::duration<double, std::milli> difference = end - start;
+  std::cout << finalOutput << std::endl;
   std::cerr << difference.count() << std::endl;
   return 0;
 }
